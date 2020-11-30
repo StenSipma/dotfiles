07f46dfcaa2e06da9a8268ce6fedf5fa2b759cff
Sten Sipma <sten.sipma@ziggo.nl>
2020-11-30 20:03:51 +0100

    Modifications (added stalonebar as status bar)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -95,26 +95,36 @@ myWorkspaces = [ "Main"
 myManageHook = composeAll [ className =? "firefox"     --> doF (W.shift "Browser")
                           , className =? "Emacs"       --> doF (W.shift "Editor")
                           , className =? "Thunderbird" --> doF (W.shift "Email")
-                          , className =? "spotify"     --> doF (W.shift "Music")
+                          -- Match Spotify, no classname given on startup!
+                          , className =? ""            --> doF (W.shift "Music")
 			  ]
 
 -- Main xmobar run sequence
 main = do
-	-- Start the wallpaper manager using the previous config
-	nitrogen <- spawn "nitrogen --restore &"
-	-- Start a system tray for some applications (e.g. NetworkManager)
-	--tray <- spawn "stalonetray &"
-	-- Start the status bar using the specified config.
-	-- The result of the spawned process is given to the log pretty printer (PP)
+        -- Start the wallpaper manager using the previous config
+        nitrogen <- spawn "nitrogen --restore &"
+        -- Start a system tray for some applications (e.g. NetworkManager)
+        -- Options:
+        -- geometry: [widthxheight]+x+y
+        --           width and height are the default size for icon 
+        --           +x is shift right by x (pixels?), opposite for -x (will wrap around)
+        -- background: colour
+        -- grow-gravity: direction for the bar to grow into, [N, W, S, E] or any 
+        --               combination of them (i.e NE, to grow right and bottom)
+        tray <- spawn "stalonetray --geometry 6x1+1330+2 --icon-gravity NE --grow-gravity NW --background \"#2f343f\" &"
+        -- Start the tray applet for NetworkManager. Might error if using wicd ?
+        networkManager <- spawn "nm-applet &"
+        -- Start the status bar using the specified config.
+        -- The result of the spawned process is given to the log pretty printer (PP)
         xmproc <- spawnPipe "xmobar $XDG_CONFIG_HOME/xmobar/xmobarrc"
         xmonad $ desktopConfig
-        -- Dictionary with custom values and hooks
-	        -- manageDocks     will shift the screen so the statusbar is visible
-		-- insertPosition  determines where a new window is spawned on the page.
+                -- Dictionary with custom values and hooks
+                    -- manageDocks     will shift the screen so the statusbar is visible
+                -- insertPosition  determines where a new window is spawned on the page.
                 { manageHook = manageDocks <+> insertPosition End Newer <+> myManageHook <+> manageHook def
                 , layoutHook = avoidStruts  $ layoutHook def
                 , logHook    = dynamicLogWithPP $ myPP xmproc
-		, workspaces = myWorkspaces
+                , workspaces = myWorkspaces
                 , terminal           = myTerminal
                 , modMask            = myModMask
                 , borderWidth        = myBorderWidth
